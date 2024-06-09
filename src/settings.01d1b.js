ce67188354736588c8ee6cd4f5a798010b32766c
haotxp <623769279@qq.com>
2024-06-09 23:20:59 +0800

    add

---
+++ src/settings.01d1b.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/LoadScene.fire",orientation:"",jsList:[],bundleVers:{internal:"620a7",main:"0c007"}};
